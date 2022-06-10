9663d59148a6a179716aa9990b95c71a06f5f34a
Neko Asakura <neko.asakura@outlook.com>
2022-06-10 21:41:30 +0100

    initial

---
+++ shoplite/src/main/java/com/shopify/shoplite/ShopliteApplication.java
@@ -0,0 +1,13 @@
+package com.shopify.shoplite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopliteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopliteApplication.class, args);
+    }
+
+}
